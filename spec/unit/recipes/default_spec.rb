0524b5e0dffcfb33eb4e3dc8e620ec7a339776ca
Gonzalo Lopez <gmlp.24a@gmail.com>
2018-12-07 15:10:50 -0600

    INIT

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook:: dou_vault_installer
+# Spec:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'dou_vault_installer::default' do
+
+  context 'CentOS 7.4' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+    it 'installs httpd' do
+      expect(chef_run).to install_package('httpd')
+    end
+
+    it 'enables httpd' do 
+      expect(chef_run).to enable_service('httpd')
+    end
+
+    it 'starts the httpd service' do
+      expect(chef_run).to start_service('httpd')
+    end
+  end
+end
